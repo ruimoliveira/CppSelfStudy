48b40d7706fdf376b775bf984aafdba284d300dc
Rui Miguel Oliveira <18540561+ruimoliveira@users.noreply.github.com>
2025-11-24 18:01:30 +0000

    adds auto keyword examples

--- cpp11/cpp11.cpp
+++ cpp11/cpp11.cpp
@@ -15,6 +15,11 @@ void Cpp11::demo() {
     braceOrEqualInitializersDemo();
 }
 
+struct Point {
+    int x, y;
+    Point(int x_val, int y_val) : x(x_val), y(y_val) {};
+};
+
 void Cpp11::braceOrEqualInitializersDemo() {
 
     std::string str_eq = "equal sign initialization";
@@ -23,11 +28,24 @@ void Cpp11::braceOrEqualInitializersDemo() {
 
     std::vector<int> vec_eq = {1, 2, 3, 4, 5}; // equal sign list initialization
 
-    struct Point {
-        int x, y;
-        Point(int x_val, int y_val) : x(x_val), y(y_val) {};
-    };
-
     Point p1 = Point(10,10); // equal sign initialization
     Point p2 {30, 40};   // brace initialization
+}
+
+void Cpp11::autoKeywordDemo() {
+    auto x{1}; // infers type int
+    const auto y = 3.14; // infers type const double
+    auto cstring = "char*"; // infers type char*
+    auto b = true; // infers type bool
+    auto p = Point(5, 10); // infers type Point
+
+    auto ptr = &p; // infers type Point*
+    auto& ref = x; // infers type int&
+    auto&& rvalueRef = 2.71; // infers type int&&
+    
+    auto list = {1, 2, 3, 4, 5}; // infers type std::initializer_list<int>
+    auto it = list.begin(); // infers type std::initializer_list<int>::iterator
+    
+    auto func = [](int a, int b) { return a + b; }; // infers type of lambda function
+    auto funcPtr = &Cpp11::demo; // infers type void (Cpp11::*)()
 }
