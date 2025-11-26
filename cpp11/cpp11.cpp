a402575ec704819d2852e8301417e029b5cf908f
Rui Miguel Oliveira <18540561+ruimoliveira@users.noreply.github.com>
2025-11-26 08:58:34 +0000

    adds examples of decltype usage and trailing return type function

--- cpp11/cpp11.cpp
+++ cpp11/cpp11.cpp
@@ -13,6 +13,8 @@ Cpp11& Cpp11::getInstance() {
 
 void Cpp11::demo() {
     braceOrEqualInitializersDemo();
+    autoKeywordDemo();
+    decltypeKeywordDemo();
 }
 
 struct Point {
@@ -21,7 +23,6 @@ struct Point {
 };
 
 void Cpp11::braceOrEqualInitializersDemo() {
-
     std::string str_eq = "equal sign initialization";
     std::string str_br {"brace initialization"};
     std::string str_lst {'l', 'i', 's', 't', ' ', 'i', 'n', 'i', 't',};
@@ -48,4 +49,28 @@ void Cpp11::autoKeywordDemo() {
     
     auto func = [](int a, int b) { return a + b; }; // infers type of lambda function
     auto funcPtr = &Cpp11::demo; // infers type void (Cpp11::*)()
+}
+
+template<typename T>
+struct GenericClass {
+    T value;
+    public:
+    GenericClass(T val) : value(val) {}
+    const T& getValue() const { return value; }
+    void setValue(T val) { value = val; }
+};
+
+template<typename T1, typename T2>
+auto add(T1 a, T2 b) -> decltype(a + b) { // trailing return type
+    return a + b;
+}
+
+void Cpp11::decltypeKeywordDemo() {
+    auto integer = add(5, 10); // infers type int
+    auto floating = add(3.14, 2.71); // infers type double
+    auto mixed = add(5, 2.71); // infers type double
+    auto str = add(std::string("Hello, "), std::string("World!")); // infers type std::string
+
+    GenericClass<int> intObj(42);
+    decltype(intObj.getValue()) intRef = intObj.getValue(); // infers type const int&
 }
